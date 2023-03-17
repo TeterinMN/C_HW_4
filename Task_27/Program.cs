eec057c93dccde1364ce342e67d969e16059fe51
Михаил Тетерин <scorpio@MacBook-Pro-Mihail.local>
2023-03-17 23:21:32 +0700

    Написал код к задаче №27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        int sum = 0;
+        while (n > 0) 
+        {
+            sum = n + n % 10;
+            n = n / 10;
+        }
+
+        Console.WriteLine("Сумма цифр в числе: " + sum);
