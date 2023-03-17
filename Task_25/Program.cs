5946bf9e2253dda5c3966bc6ceff495b0a3ea6f5
Михаил Тетерин <scorpio@MacBook-Pro-Mihail.local>
2023-03-17 22:19:17 +0700

    Написал код к заданию №25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите два числа: ");
+int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+int result = x;
+
+
+for (int i = 1; i < y; i++)
+{
+    result = result * x;
+}
+ Console.WriteLine(result);
